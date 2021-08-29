3f49a99340bd34c13966ff7d4dadebc88ddc98ef
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2021-08-29 13:05:37 +0100

    Add negative inputs for negative output

--- Fraction-java/test/source/math/AddFractionsTest.java
+++ Fraction-java/test/source/math/AddFractionsTest.java
@@ -29,4 +29,10 @@ public class AddFractionsTest {
         Fraction sum = new Fraction(3).plus(new Fraction(4));
         assertEquals(7, sum.intValue());
     }
+
+    @Test
+    public void negativeInputsAndNegativeOutput() {
+        Fraction sum = new Fraction(-3).plus(new Fraction(1));
+        assertEquals(-2, sum.intValue());
+    }
 }
