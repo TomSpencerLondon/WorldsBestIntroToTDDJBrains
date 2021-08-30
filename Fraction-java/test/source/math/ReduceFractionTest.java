024f49fea0ffa97d98f44add302ebb55ec3e7eb5
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2021-08-30 11:54:45 +0100

    add gcd algorithm

---
+++ Fraction-java/test/source/math/ReduceFractionTest.java
@@ -0,0 +1,17 @@
+package source.math;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReduceFractionTest {
+    @Test
+    public void alreadyInLowestTerms() {
+        assertEquals(new Fraction(3, 4), new Fraction(3, 4));
+    }
+
+    @Test
+    public void reduceToNotWholeNumber() {
+        assertEquals(new Fraction(3, 4), new Fraction(6, 8));
+    }
+}
