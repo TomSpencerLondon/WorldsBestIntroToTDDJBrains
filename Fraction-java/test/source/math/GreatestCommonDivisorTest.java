3a3ff153365366fdddcfbdb447fbc872f41d1181
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2021-08-30 12:03:32 +0100

    add Math.abs

--- Fraction-java/test/source/math/GreatestCommonDivisorTest.java
+++ Fraction-java/test/source/math/GreatestCommonDivisorTest.java
@@ -9,14 +9,14 @@ public class GreatestCommonDivisorTest {
     public void reflexive() {
         assertEquals(1, gcd(1, 1));
         assertEquals(2, gcd(2, 2));
-        assertEquals(-1, gcd(-1, -1));
+        assertEquals(1, gcd(-1, -1));
     }
 
     @Test
     public void relativelyPrime() {
         assertEquals(1, gcd(2, 3));
         assertEquals(1, gcd(4, 7));
-        assertEquals(-1, gcd(-2, -3));
+        assertEquals(1, gcd(-2, -3));
     }
 
     @Test
@@ -45,6 +45,6 @@ public class GreatestCommonDivisorTest {
             b = a % t;
             a = t;
         }
-        return a;
+        return Math.abs(a);
     }
 }
