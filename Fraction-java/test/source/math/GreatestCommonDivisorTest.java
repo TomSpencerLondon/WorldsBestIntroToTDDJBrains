de8229dc2d517877d8f625e936022f9fbe0f4b15
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2021-08-30 12:02:41 +0100

    Explore gcd - not like negatives

--- Fraction-java/test/source/math/GreatestCommonDivisorTest.java
+++ Fraction-java/test/source/math/GreatestCommonDivisorTest.java
@@ -6,8 +6,37 @@ import static org.junit.Assert.assertEquals;
 
 public class GreatestCommonDivisorTest {
     @Test
-    public void oneAndone() {
+    public void reflexive() {
         assertEquals(1, gcd(1, 1));
+        assertEquals(2, gcd(2, 2));
+        assertEquals(-1, gcd(-1, -1));
+    }
+
+    @Test
+    public void relativelyPrime() {
+        assertEquals(1, gcd(2, 3));
+        assertEquals(1, gcd(4, 7));
+        assertEquals(-1, gcd(-2, -3));
+    }
+
+    @Test
+    public void oneIsMultipleOfTheOther() {
+        assertEquals(3, gcd(3, 9));
+        assertEquals(5, gcd(5, 30));
+    }
+
+    @Test
+    public void commonFactor() {
+        assertEquals(2, gcd(6, 8));
+        assertEquals(7, gcd(49, 315));
+    }
+
+    @Test
+    public void negative() {
+        // I'm not sure whether I like this behaviour.
+        // I probably prefer that GCD be positive at all times
+        assertEquals(4, gcd(-24, 28));
+
     }
 
     private int gcd(int a, int b) {
